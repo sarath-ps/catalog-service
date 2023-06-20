75a413fe8bbc63806b9dfc3850ada46ffbda325e
Sarath P S <hello@sarath.info>
2023-06-20 20:12:20 +0530

    docker image config

--- postgresql.sh
+++ postgresql.sh
@@ -1,8 +1,9 @@
-docker run  --name catalog-app \
+docker run  --name catalog-db \
   --rm \
+  --net catalog-network \
   -e POSTGRES_USER=user \
   -e POSTGRES_PASSWORD=password \
-  -e POSTGRES_DB=catalog_app \
+  -e POSTGRES_DB=catalog_db \
   -p 5432:5432 \
   postgres:14.4
 
